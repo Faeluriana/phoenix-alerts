abacb238f331226218d6544f86818c6cf1df77cb
Faeluriana <150099197+Faeluriana@users.noreply.github.com>
2023-11-11 13:35:03 +0000

    Some Fixes | Added translations | Better debug

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # Phoenix Alerts
-- Alerts you when a party member is ressurecting with a Phoenix passive or with an Eren's Key.
-- If your class is not a healer, it only alerts you when the healer is resurrecting with phoenix/key.
-- If your class is a healer, it alerts you for everyone.
+- Alerts you with a fake party message when a party member is resurrecting with a Phoenix passive or with an Eren's Key
+- Only you will be able to see this message
+- If your class is not a healer, it only alerts you when the healer is resurrecting with phoenix/key
+- If your class is a healer, it alerts you for everyone
 
-# Configuration
+## Configuration
 - `enabled`: `true` if you want the mod enabled on startup. `false` otherwise
-- `everyone`: `true` if you want the mod to alert you about everyone, regardless of your class.
-- - Only meaningful when you are not a Healer.
-- `debug`: `true` if you want debug messages. `false` otherwise
-- `dungeon`: `true` if you only want it to work when you're inside a dungeon.
+- `everyone`: `true` if you want the mod to alert you about everyone, regardless of your class
+- - Only meaningful when you are not a Healer
+- `debug`: `true` if you want debug messages
+- `dungeon`: `true` if you only want it to work when you're inside a dungeon
+- `language`: `en` for English language
 
-# Translations
-- If you wish to help translate to your language i'd appreciate it! It's a very small mod so it should be very easy
-
-# Dependencies
+## Dependencies
 - This mod was made for **[Tera Private Toolbox](https://github.com/tera-private-toolbox/tera-toolbox)** - I cannot make any guarantees that it will work on any other toolbox
 - **[Command](https://github.com/tera-private-toolbox/command)** - Already installed in toolbox
 - **[Tera-Game-State](https://github.com/tera-private-toolbox/tera-game-state)**
+
+## Translations
+- If you wish to help translate to your language i'd appreciate it! It's only a few words so it should be very easy. Some current translations might be wrong... >.<
+
+## Special Thanks
+- to Chiral for helping with the testing
--- index.js
+++ index.js
@@ -7,14 +7,15 @@ module.exports = function phoenix(mod) {
     let debug = config.debug;
     let everyone = config.everyone;
     let dungeonOnly = config.dungeon;
-    let myID = null;
     let healer = null;
+    let lang = config.language;
+    let msgs = config.availableLanguages[lang];
 
     mod.command.add('phoenix', (cmd) => {
         switch (cmd) {
             case "all":
                 everyone = !everyone;
-                mod.command.message('phoenix alerting when ' + (everyone ? 'anyone' : (healer ? 'else' : 'healer') + ' is resurrecting'));
+                mod.command.message('phoenix alerts when ' + (everyone ? 'anyone': (healer ? '' : 'a healer')) + ' is resurrecting');
                 break;
             case "debug":
                 debug = !debug;
@@ -28,20 +29,19 @@ module.exports = function phoenix(mod) {
 
     // On login
     mod.game.on('enter_game', () => {
-        myID = mod.game.me.gameId;
         healer = Healers.includes(mod.game.me.class);
     });
 
     // On logout
     mod.game.on('leave_game', () => {
-        myID = null;
         healer = null;
     });
 
     // S_CREATURE_LIFE -> Resurrection
     mod.hook('S_CREATURE_LIFE', 3, (event) => {
         if (debug) {
-            mod.command.message('S_CREATURE_LIFE: ' + event.gameId + ' ' + event.alive);
+            const name = mod.game.party.getMemberData(event.gameId).name;
+            mod.command.message('S_CREATURE_LIFE: ' + name + ' ' + (event.alive ? 'alive' : 'dead'));
             mod.command.message('resItem: ' + event.resItem + ' | resPassive: ' + event.resPassive);
         }
 
@@ -49,42 +49,41 @@ module.exports = function phoenix(mod) {
         if (mod.game.party.isMember(event.gameId) && !mod.game.me.is(event.gameId)) {
             if (!event.alive) {
                 // Phoenix mount passive
-                if (event.resPassive && canSendMessage(event.gameId)) {
-                    if (debug) mod.command.message('Phoenix Resurrection');
-                    sendFakeMessage(event.gameId, 'Phoenix Resurrection');
+                if (event.resPassive && canSendMessage(event.gameId)) {                    
+                    sendFakeMessage(event.gameId, msgs[0]);
                 }
             }
             else { // event.alive
                 // Eren's key
                 if (event.resItem && canSendMessage(event.gameId)) {
-                    if (debug) mod.command.message('Using Eren\'s Key');
-                    sendFakeMessage(event.gameId, 'Using Eren\'s Key');
+                    sendFakeMessage(event.gameId, msgs[1]);
                 }
-
             }
         }
-
     });
 
     const canSendMessage = (gameId) => {
         let targetRole = mod.game.party.getMemberData(gameId).class;
+        const name = mod.game.party.getMemberData(gameId).name;
 
+        if (debug){
+            const name = mod.game.party.getMemberData(gameId).name;
+            mod.command.message('canSendMessage: ' + name + ' ' + targetRole);
+        }
         if (!enabled) return false;
         if (dungeonOnly && !mod.game.me.inDungeon) {
             return false;
         }
-
+        
         // Protect against unknown scenarios
         if (mod.game.me.inBattleground || mod.game.me.inCivilUnrest) return false;
 
         if (everyone || healer) return true;
-
         else if (!healer) { // If you are not a healer
             if (!Healers.includes(targetRole)) {
                 return false; // and target is not a healer
             }
         }
-
         return true;
     };
 
@@ -92,10 +91,6 @@ module.exports = function phoenix(mod) {
         let person = mod.game.party.getMemberData(gameId);
         let pname = person.name;
 
-        if (debug) {
-            mod.command.message('sendFakeMessage from ' + pname + ': ' + message);
-            //return;
-        }
         mod.send('S_CHAT', 3, {
             channel: 1, // 1 = party
             gameId: gameId, // Might be useless
@@ -108,5 +103,4 @@ module.exports = function phoenix(mod) {
             message: message
         });
     }
-
 };
